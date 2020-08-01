1ffcecb7d5ff39e9e95342c546df3ca4b7bbccbb
Pedro <pedropaulo3521@gmail.com>
2020-08-01 20:34:37 -0300

    novo

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '../../assets/img/logo.png'
+import logo from '../../assets/img/gn_logo.png'
 import ButtonLink from './components/ButtonLink';
 import { Link } from 'react-router-dom';
 import '../../Menu.css';
 
 const Nav = () =>{
-
+    // teste
     return(
         <nav className='Menu'>
             <Link to='/'><img src={logo} className='Logo' alt='cypunflix logo'/></Link>
