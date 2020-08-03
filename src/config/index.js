602e8613e514374cd1556f3ae88f022a46328a86
Pedro <pedropaulo3521@gmail.com>
2020-08-03 17:08:31 -0300

    mudado o arquivo config para caso esteja em dev, usar outro endereco pro back

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,8 @@
 //variaveis de ambiente para fazer requisicoes pro backend
 
-const URL_BACKEND = 'https://gostos-nerds.herokuapp.com';
+const URL_BACKEND = window.location.hostname.includes('localhost')
+  ? 'http://localhost:8080'
+  : 'https://gostos-nerds.herokuapp.com';
 
 export default {
   URL_BACKEND,
